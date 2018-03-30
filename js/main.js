2451e48deaa36879c8fff4bfbd4cf263a4ff52d1
Yufei Liu <kevenliu94@gmail.com>
2018-03-29 21:27:58 -0400

    Added search module

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "50%";
-    document.getElementById("page").style = "transition: 1s;filter:blur(2px);"
+    document.getElementById("page").style = "transition: 0.7s;filter:blur(2px);"
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("page").style = "transition: 1s;filter:blur(0px);"
+    document.getElementById("page").style = "transition: 0.7s;filter:blur(0px);"
 }
+
+function myFunction() {
+  var input, filter, project, p, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  project = document.getElementById("project");
+  p = project.getElementsByTagName("P");
+  for (i = 0; i < p.length; i++) {
+      if (p[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        p[i].style.display = "";
+      } else {
+        p[i].style.display = "none";
+      }  
+  }
+}
+
 
